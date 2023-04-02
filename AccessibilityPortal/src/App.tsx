be5abdc97a3395e2b8bedef1e474910d77b5a5ab
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-04-02 14:35:43 -0500

    added sass and bootstrap packages

--- AccessibilityPortal/src/App.tsx
+++ AccessibilityPortal/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import ReactDOM from "react-dom/client";
-import './Styles/App.css'
+import './Styles/App.scss'
 import React from 'react'
 import Registration from './Pages/Registration/registration';
 import LoginPage from './Pages/Login/login-page';
