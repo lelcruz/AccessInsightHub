cebebc2c40ecd25df5fa90e177e2c7ea53dd0209
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-09-03 15:31:21 -0500

    Created a basic navbar and added it to the research page

---
+++ AccessibilityPortal/src/CommonComponents/Navbar/NavbarComponent.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './NavbarComponent.scss';
+
+
+const NavbarComponent = () => {
+    return (
+            <nav className="navbar bg-body-tertiary" style={{backgroundColor: "lightgrey"}}>
+                <div className="container-md">
+                    <Link to="/main" className="nav-link active" aria-current="page">Home</Link>
+                    <Link to="/register" className="nav-link active" aria-current="page">Surveys</Link>
+                    <Link to="/main" className="nav-link active" aria-current="page">Studies</Link>
+                    <Link to="/research" className="nav-link active" aria-current="page">Profile</Link>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button className="btn btn-dark" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+
+
+    );
+};
+
+export default NavbarComponent;
--- AccessibilityPortal/src/Pages/Research/ResearchPage.tsx
+++ AccessibilityPortal/src/Pages/Research/ResearchPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import '../../Styles/ResearchPage.scss';
+import NavbarComponent from "../../CommonComponents/Navbar/NavbarComponent";
 function ResearchPage(){
 
     return (
         <div className="main-page">
+            <NavbarComponent/>
+
 
 
 
