fef47c4e34b15ee2fab5b395b0c295004e73eef7
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-09-05 11:52:56 -0500

    Added a handling event into the submit buttion on the navbar

--- AccessibilityPortal/src/App.tsx
+++ AccessibilityPortal/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './Styles/App.scss'
 import React from 'react'
-import Registration from './Pages/Registration/registration';
+import RegistrationPage from './Pages/Registration/RegistrationPage';
 import LoginPage from './Pages/Login/login-page';
 import MainPage from './Pages/MainPage/main-page';
 import ResearchPage from "./Pages/Research/ResearchPage";
@@ -14,7 +14,7 @@ function App() {
       <Routes>
           <Route path='/' element={<LoginPage />}/>
           <Route path='/login' element={<LoginPage />}/>
-          <Route path='/register' element={<Registration />}/>
+          <Route path='/register' element={<RegistrationPage />}/>
           <Route path='/main' element={<MainPage />}/>
           <Route path='/research' element={<ResearchPage />}/>
 
--- AccessibilityPortal/src/CommonComponents/Navbar/NavbarComponent.tsx
+++ AccessibilityPortal/src/CommonComponents/Navbar/NavbarComponent.tsx
@@ -4,15 +4,19 @@ import './NavbarComponent.scss';
 
 
 const NavbarComponent = () => {
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log('Submit!');
+    };
     return (
             <nav className="navbar bg-body-tertiary">
                 <div className="container-md">
 
                     <Link to="/main" className="nav-link active" aria-current="page">Home</Link>
-                    <Link to="/register" className="nav-link active" aria-current="page">Surveys</Link>
-                    <Link to="/main" className="nav-link active" aria-current="page">Studies</Link>
-                    <Link to="/research" className="nav-link active" aria-current="page">Profile</Link>
-                    <form className="d-flex" role="search">
+                    <Link to="/" className="nav-link active" aria-current="page">Surveys</Link>
+                    <Link to="/research" className="nav-link active" aria-current="page">Studies</Link>
+                    <Link to="/" className="nav-link active" aria-current="page">Profile</Link>
+                    <form onSubmit={submitHandler} className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                             <button className="btn btn-dark" type="submit">Search</button>
                     </form>
