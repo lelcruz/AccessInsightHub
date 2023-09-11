47d211956c6aca969d13e3a07318d100d37583b5
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-09-11 09:35:23 -0500

    Created post studies page with form validation

--- AccessibilityPortal/src/Pages/Research/ResearchPage.tsx
+++ AccessibilityPortal/src/Pages/Research/ResearchPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../Styles/ResearchPage.scss';
 import NavbarComponent from "../../CommonComponents/Navbar/NavbarComponent";
+import StudyForm from "./StudyForm";
 function ResearchPage(){
 
     return (
         <div className="ResearchPageBody">
             <NavbarComponent/>
+            <StudyForm/>
 
 
 
