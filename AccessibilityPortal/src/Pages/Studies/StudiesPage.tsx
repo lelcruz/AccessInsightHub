76c5cb3220573c0fb46924476e6050f374f57da8
Leslie Cruz <lesliecruz2@my.unt.edu>
2023-10-09 14:29:56 -0500

    created pagination for the studies page

--- AccessibilityPortal/src/Pages/Studies/StudiesPage.tsx
+++ AccessibilityPortal/src/Pages/Studies/StudiesPage.tsx
@@ -52,17 +52,8 @@ function StudiesPage() {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
-    {
-      id: 3,
-      title: "Studies title 3",
-      author: "Author Name 3",
-      studyType: "N/A",
-      date: new Date("01/28/2023"),
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
   ];
-  const studiesPerPage = 1;
+  const studiesPerPage = 2;
   const [activePage, setActivePage] = useState(1);
   const indexOfLastStudy = activePage * studiesPerPage;
   const indexOfFirstStudy = indexOfLastStudy - studiesPerPage;
