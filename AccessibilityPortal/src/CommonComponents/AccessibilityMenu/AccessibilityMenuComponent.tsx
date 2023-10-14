4ec591232973d687df1d3dfb2c0cf56052f6805a
snewbz02 <snewbz02@gmail.com>
2023-10-13 22:20:47 -0500

    Test Button
    Created a test button for now, will add all buttons for menu next.

--- AccessibilityPortal/src/CommonComponents/AccessibilityMenu/AccessibilityMenuComponent.tsx
+++ AccessibilityPortal/src/CommonComponents/AccessibilityMenu/AccessibilityMenuComponent.tsx
@@ -1,34 +1,43 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import accessibilityIcon from "../../assets/universal-access-svgrepo-com.svg";
 import "./AccessibilityMenu.scss";
 
 function AccessibilityMenu() {
+    const [toggle, setToggle] = useState(false);
+    const [showTestButton, setShowTestButton] = useState(true);
 
-    const [toogle, setToogle] = useState(false)
-
-    const menuRef = useRef<HTMLDivElement>(null)
+    const menuRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const clickOutside = (e: MouseEvent) => {
-            if (toogle && menuRef.current && !menuRef.current.contains(e.target as Node)) {
-                setToogle(false)
+            if (toggle && menuRef.current && !menuRef.current.contains(e.target as Node)) {
+                setToggle(false);
             }
-        }
-        //Close the menu on mousedown outside the menu
-        document.addEventListener("mousedown", clickOutside)
-    }, [toogle])
+        };
+        // Close the menu on mousedown outside the menu
+        document.addEventListener("mousedown", clickOutside);
 
-    return(
+        // Clean up the event listener on component unmount
+        return () => {
+            document.removeEventListener("mousedown", clickOutside);
+        };
+    }, [toggle]);
+
+    return (
         <div ref={menuRef}>
-            <button className="logo-button" onClick={() => setToogle(!toogle)}>
-                <img src={accessibilityIcon} className="img-bottom-right"></img>
+            <button className="logo-button" onClick={() => setToggle(!toggle)}>
+                <img src={accessibilityIcon} className="img-bottom-right" alt="Accessibility Icon" />
             </button>
-            <div className={`accessibility-menu${toogle ? ' show-menu' : ''}`}></div>
-            
+            <div className={`accessibility-menu${toggle ? ' show-menu' : ''}`}>
+                {showTestButton && ( // Render the test button only if showTestButton is true
+                    <button className="test-button" onClick={() => alert("Test button clicked!")}>
+                        Test
+                    </button>
+                )}
+            </div>
         </div>
-
-    )
-
+    );
 }
 
 export default AccessibilityMenu;
+
