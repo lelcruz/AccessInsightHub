5795647b1dc6c79210902a8e9f0535caf834a3f2
Shane Luong <94140822+shaneluongbkk@users.noreply.github.com>
2023-12-03 14:22:41 -0600

    Delete hashPassword.tsx

--- AccessibilityPortal/src/Backend/hashPassword.tsx
+++
@@ -1,9 +0,0 @@
-import { createHash } from 'crypto';
-
-function hashPassword(password: string): string {
-    const sha256 = createHash('sha256');
-    sha256.update(password, 'utf8');
-    return sha256.digest('hex');
-}
-
-export { hashPassword };
